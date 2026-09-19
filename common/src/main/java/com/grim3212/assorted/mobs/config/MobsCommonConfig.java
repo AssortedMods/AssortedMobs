cb1a84956be13db0bf36b815e6747ae99ec369ad
grim3212 <dakotapierone@yahoo.com>
2026-09-19 16:28:08 -0700

    Bump default ice pixie amount

--- common/src/main/java/com/grim3212/assorted/mobs/config/MobsCommonConfig.java
+++ common/src/main/java/com/grim3212/assorted/mobs/config/MobsCommonConfig.java
@@ -29,7 +29,7 @@ public class MobsCommonConfig {
         treasureMobEnabled = builder.defineBoolean("parts.treasureMobEnabled", true, "Set this to true if you would like treasure mobs to spawn and be found in the creative tab.");
         eightBitMobsEnabled = builder.defineBoolean("parts.eightBitMobsEnabled", true, "Set this to true if you would like parabuzzies to spawn, Bob-ombs to be craftable and both to be found in the creative tab.");
 
-        icePixieWeight = builder.defineInteger("spawning.icePixieWeight", 20, 0, 1000, "How often ice pixies spawn in snowy biomes, against the other monsters there. Zombies are 95. Set to 0 to stop them spawning.");
+        icePixieWeight = builder.defineInteger("spawning.icePixieWeight", 39, 0, 1000, "How often ice pixies spawn in snowy biomes, against the other monsters there. Those add up to 515, so 39 is 7% of monsters. Set to 0 to stop them spawning.");
         treasureMobWeight = builder.defineInteger("spawning.treasureMobWeight", 5, 0, 1000, "How often treasure mobs spawn inside the structures in the assortedmobs:spawns_treasure_mobs structure tag. Set to 0 to stop them spawning.");
         parabuzzyWeight = builder.defineInteger("spawning.parabuzzyWeight", 8, 0, 1000, "How often parabuzzies spawn in the overworld, against the other animals there. A higher weight is more common. Set to 0 to stop them spawning.");
 
